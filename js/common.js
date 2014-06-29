74fceed05cb0cc39e6e70b4e5a88d5d448adf25b
akorol777 <akorol777@ya.ru>
2014-06-29 13:30:00 +0300

    centr__half

---
+++ js/common.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+
+
+$(function() {
+    $( ".ring__head" ).click(function() {
+      $( ".ring__popup" ).toggleClass( "is-open");
+      return false;
+    });
+  });
+
+$(function() {
+    $( ".btn-popup" ).click(function() {
+      $( ".send__popup" ).toggleClass( "send__popup-active");
+      return false;
+    });
+  });
+$(function() {
+    $( ".send__popupclose" ).click(function() {
+      $( ".send__popup" ).removeClass( "send__popup-active");
+      return false;
+    });
+  });
+
+
+});
